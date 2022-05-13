d0f33f29957b016f5b4fdc2ad21be53711ff5458
Max Oblein <max.oblein@gmail.com>
2022-05-13 15:50:02 +0100

    initialised database

--- website/models.py
+++ website/models.py
@@ -9,7 +9,7 @@ class Note(db.Model):
     userID = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integr, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
