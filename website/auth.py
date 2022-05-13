e309be4c309a794a4e32ce07b118a53e41f9cf8c
Max Oblein <max.oblein@gmail.com>
2022-05-13 16:15:12 +0100

    Added user creation functionality

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from sqlalchemy import true
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth',__name__)
 
@@ -31,7 +34,12 @@ def sign_up():
             flash('Password must be at least 7 characters', category='error')
         else:
             #add user to db
+            new_user = User(email=email,firstName=firstName, password= generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('Account created!',category='success')
+            return redirect(url_for('views.home'))
             
 
     return render_template('signUp.html')
